a76eba567bf7c77124d6cd7b45747a5c9498de52
Gibraan Rahman <grahman@eng.ucsd.edu>
2019-02-15 16:59:28 -0800

    fixed issue with follow up days. now follow_up_counts has status and days_to_last_followup or days_to_death as appropriate

--- clinical/extra_clin.py
+++ clinical/extra_clin.py
@@ -12,23 +12,30 @@ def extract_days(uuid):
     uuid_dir = '%s/%s' % (CLIN_DIR, uuid)
     filename = glob.glob('%s/*.xml' % uuid_dir)[0]
     follow_up_days = []
+    status = 'Alive'
     with open(filename, 'r') as f:
         for line in f:
             if re.search('vital_status', line):
-                if re.search('Dead', line):
-                    return([])
-                    continue
+                if re.search('dead', line, re.IGNORECASE):
+                    status = 'Dead'
+            if re.search('days_to_death', line):
+                days = re.search("(?<=>)[0-9]*(?=<)", line)
+                if days:
+                    return('Dead', days.group())
             if re.search('days_to_last_followup', line):
                 days = re.search("(?<=>)[0-9]*(?=<)", line)
                 if days:
                     follow_up_days.append(days.group())
-    return(list(map(int, follow_up_days)))
+    follow_up_days = list(map(int, follow_up_days))
+    if not follow_up_days:
+        return(status, 'NA')
+    return('Alive', max(follow_up_days))
 
 if __name__ == '__main__':
     os.chdir('/home/grahman/projects/rotation_2019/clinical')
     manifest_filename = '../data/extra_clin/gdc_manifest.2019-02-14.txt'
     count = 0
-    follow_up_dict = {}
+    day_dict = {}
     with open(manifest_filename, 'r') as f:
         for line in f:
             xml = re.search('xml', line)
@@ -39,9 +46,8 @@ if __name__ == '__main__':
             barcode = re.search('TCGA-.{2}-.{4}', line).group()
             uuid = re.match('[0-9a-z\-]*', line).group()
             follow_up_days = extract_days(uuid)
-            if follow_up_days:
-                follow_up_dict[barcode] = max(follow_up_days)
-    
+            day_dict[barcode] = follow_up_days
+
     with open('follow_up_counts.txt', 'w+') as f:
-        for key, item in follow_up_dict.items():
-            f.write('%s\t%s\n' % (key, item))
+        for key, item in day_dict.items():
+            f.write('%s\t%s\t%s\n' % (key, item[0], item[1]))
