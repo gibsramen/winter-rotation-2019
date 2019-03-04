0b06e4c60fe2f124ac6b75f8e40b9e9efd358a65
Gibraan Rahman <grahman@eng.ucsd.edu>
2019-03-04 11:10:27 -0800

    updated extra clinical stuff to properly extract days or label sample status and/or days as NA

--- clinical/extra_clin.py
+++ clinical/extra_clin.py
@@ -6,18 +6,22 @@ import glob
 import os
 import re
 
-CLIN_DIR = '../data/extra_clin/clin_dirs'
+CLIN_DIR = '/home/grahman/data/TCGA_clin/clin_dirs'
 
 def extract_days(uuid):
     uuid_dir = '%s/%s' % (CLIN_DIR, uuid)
     filename = glob.glob('%s/*.xml' % uuid_dir)[0]
     follow_up_days = []
-    status = 'Alive'
+    status = ''
+    statuses = []
     with open(filename, 'r') as f:
         for line in f:
             if re.search('vital_status', line):
                 if re.search('dead', line, re.IGNORECASE):
                     status = 'Dead'
+                if re.search('alive', line, re.IGNORECASE):
+                    status = 'Alive'
+                statuses.append(status)
             if re.search('days_to_death', line):
                 days = re.search("(?<=>)[0-9]*(?=<)", line)
                 if days:
@@ -27,13 +31,19 @@ def extract_days(uuid):
                 if days:
                     follow_up_days.append(days.group())
     follow_up_days = list(map(int, follow_up_days))
+    if 'Dead' in statuses:
+        status = 'Dead'
+        return('Dead', 'NA')
+    if not status:
+        return('NA', 'NA')
     if not follow_up_days:
-        return(status, 'NA')
-    return('Alive', max(follow_up_days))
+        return('Alive', 'NA')
+    return(status, max(follow_up_days))
 
 if __name__ == '__main__':
     os.chdir('/home/grahman/projects/rotation_2019/clinical')
-    manifest_filename = '../data/extra_clin/gdc_manifest.2019-02-14.txt'
+    #manifest_filename = '../data/extra_clin/gdc_manifest.2019-02-14.txt'
+    manifest_filename = '/home/grahman/data/TCGA_clin/gdc_manifest.2019-02-14.txt'
     count = 0
     day_dict = {}
     with open(manifest_filename, 'r') as f:
@@ -46,6 +56,7 @@ if __name__ == '__main__':
             barcode = re.search('TCGA-.{2}-.{4}', line).group()
             uuid = re.match('[0-9a-z\-]*', line).group()
             follow_up_days = extract_days(uuid)
+            print(follow_up_days)
             day_dict[barcode] = follow_up_days
 
     with open('follow_up_counts.txt', 'w+') as f:
